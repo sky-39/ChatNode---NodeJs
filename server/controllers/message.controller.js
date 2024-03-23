aa5bfa955fc650be1aaa14694125b0f716b3da08
Akash Tiwari <akash_t@bt.iitr.ac.in>
2024-03-23 23:56:47 +0530

    comment-removal-one-client-bug-remaining

--- server/controllers/message.controller.js
+++ server/controllers/message.controller.js
@@ -30,8 +30,6 @@ export const sendMessage = async (req, res) => {
       conversation.messages.push(newMessage._id);
     }
 
-    // await conversation.save();
-    // await newMessage.save();
 
     await Promise.all([conversation.save(), newMessage.save()]); // this will run in parallel
 
